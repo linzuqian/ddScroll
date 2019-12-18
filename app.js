081d3127f9348f1f6b62369d402400902947ed42
linzuqian <364698133@qq.com>
2019-12-18 09:03:16 +0800

    滚动demo

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 App({
-  globalData:{
-    token:''
+  globalData: {
+    token: ''
   },
   onLaunch(options) {
-    dd.getStorageSync({
-      key:"token",
-      success:(res)=>{
-        var a = 1
-        this.globalData.token = res.token
-        console.log(111)
-      },
-      fail:(err)=>{
-        var b = 3
-        var c = 3
-        console.log(222)
-      }
-    })
-    console.info('App onLaunch');
   },
   onShow(options) {
-    // 从后台被 scheme 重新打开
-    // options.query == {number:1}
+
   },
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,181 +1,41 @@
-import Dropdown from '../../components/dropdown';
-import list from '../../components/list';
-import {postTo,getTo} from '../../utils/RequestUtils.js';
-
-let app = getApp();
 Page({
-  ...Dropdown,
-  ...list,
-  data:{
-    dropdownSelectData: {
-      active: false,
-      selectedNav: 0,
-      listData: [
-        {
-          nav: '投放中',
-          selectedItem: '',
-          data: [
-            {
-              title: '投放中'
-            },
-            {
-              title: '已结束',
-            }
-          ]
-        },
-      ],
-    },
-    listData: {
-      onItemTap: 'handleListItemTap',
-      data: []
-    },
-  },
-  // drawdown开始
-  onDropdownNavItemTap(e, index) {
-    const { selectedNav, active } = this.data.dropdownSelectData;
-    let nextactive = !active;
-    var aaa = app;
-    if (selectedNav !== index) {
-      nextactive = true;
-    }
-
-    this.setData({
-      dropdownSelectData: {
-        ...this.data.dropdownSelectData,
-        active: nextactive,
-        selectedNav: index
-      }
-    });
-
-    
-  },
-  catchDropdownNavItemTap(e, parentIndex, index, title) {
-    const { listData } = this.data.dropdownSelectData;
-    const data = listData[parentIndex];
-    data.selectedItem = index;
-    this.setData({
-      dropdownSelectData: {
-        ...this.data.dropdownSelectData,
-        active: false,
-        
-      }
-    });
-    var nav = this.data.dropdownSelectData.listData[0].data[index].title;
-    this.setData({
-      ['dropdownSelectData.listData[0].nav'] : nav
-    });
-    this.downloadAdList(index)
-  },
-  catchDropdownBgTap(e) {
-    this.setData({
-      active: false
-    });
-  },
-  // drawdown结束
-
-  // list开始
-
-  // list结束
-
-  onLoad(query) {
-    // 页面加载
-    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
-
-  },
-  onReady() {
-    var a = 11;
-    // 页面加载完成
-    if(app.globalData.token != ''){
-      this.downloadAdList()
-    }else{
-      this.loginAndDownloadList()
-    }
+  data: {
+    list: [
+      { id: 111, name: "滚动1" },
+      { id: 222, name: "滚动2" },
+      { id: 333, name: "滚动3" },
+      { id: 444, name: "滚动4" },
+      { id: 555, name: "滚动5" },
+      { id: 666, name: "滚动6" },
+      { id: 777, name: "滚动7" },
+      { id: 888, name: "滚动8" },
+      { id: 999, name: "滚动9" },
+      { id: 1010, name: "滚动10" },
+      { id: 1111, name: "滚动11" },
+      { id: 1212, name: "滚动12" },
+      { id: 1313, name: "滚动13" },
+      { id: 1414, name: "滚动14" },
+      { id: 1515, name: "滚动15" },
+      { id: 1616, name: "滚动16" },
+      { id: 1717, name: "滚动17" },
+      { id: 1818, name: "滚动18" },
+    ],
   },
   onShow() {
-    // 页面显示
+    this.getGroupLists();
   },
-  onHide() {
-    // 页面隐藏
+  getGroupLists() {
+    let groupLists = this.data.list.concat(this.data.list)
+    this.setData({
+      list: groupLists
+    })
   },
-  onUnload() {
-    // 页面被关闭
+  lower(e) {
+    this.getGroupLists()
   },
-  onTitleClick() {
-    // 标题被点击
-  },
-  onPullDownRefresh() {
-    // 页面被下拉
-  },
-  onReachBottom() {
-    // 页面被拉到底部
-  },
-  onShareAppMessage() {
-    // 返回自定义分享信息
-    return {
-      title: 'My App',
-      desc: 'My App description',
-      path: 'pages/index/index',
-    };
-  },
-  loginAndDownloadList(){
-    console.info("点击登陆");
-    var that = this;
-    dd.getAuthCode({
-        success:function(res){
-          that.loginRequest(res.authCode);
-        },
-        fail:function(err){
-          console.info(err);
-        }
+  toDetails() {
+    dd.navigateTo({
+      url: "../setting/index"
     });
-  },
-  loginRequest(authCode){
-    
-    postTo("ding/login",
-    {
-      authCode:authCode
-    },
-    (res)=>{
-      if(res.data.code == 200){
-        app.globalData.token = res.data.data.token;
-        dd.setStorageSync({
-          key:"token",
-          data:app.globalData.token
-        })
-        this.downloadAdList();
-      }
-      
-      console.info(res)
-    },
-    (err)=>{
-      console.index(err)
-    })
-  },
-  downloadAdList(type = 0){
-    var status = type == 1 ? "Finished":"Normal";
-    var _this = this;
-    getTo("adManager/list",{pageSize:0,pageIndex:0,status:status},
-    (res)=>{
-      if(res.data.code == 200){
-        var showList = [];
-        for(var i = 0;i <res.data.data.list.length;i++){
-          var ad = res.data.data.list[i];
-          var title = ad.buildingName;
-          var extra = '';
-          extra += '总计：' + ad.subCount;
-          extra += ',昨日：' + ad.yestodaySubCount;
-          showList.push({title:title,extra:extra,textMode:"wrap"})
-        }
-   
-        _this.setData({
-          ['listData.data'] : showList
-        })
-      }
-
-        console.info(res)
-    },
-    (err)=>{
-
-    })
   }
-});
+})
